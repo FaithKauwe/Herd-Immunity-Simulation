d83c252e3d1911de83bad63f90323b87a525407a
FaithKauwe <fkauwe@gmail.com>
2024-12-07 19:43:58 -0800

    completed virus.py

--- virus.py
+++ virus.py
@@ -3,8 +3,9 @@ class Virus(object):
     def __init__(self, name, repro_rate, mortality_rate):
         # Define the attributes of your your virus
         self.name = name
-        # TODO Define the other attributes of Virus
-        pass
+        self.repro_rate = repro_rate
+        self.mortality_rate = mortality_rate
+       
 
 
 # Test this class
