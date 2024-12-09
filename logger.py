de0605df0caa0f91e5d1d51942f251bd4219a0c1
FaithKauwe <fkauwe@gmail.com>
2024-12-08 17:26:10 -0800

    finished logger.py

--- logger.py
+++ logger.py
@@ -1,8 +1,9 @@
 class Logger(object):
     def __init__(self, file_name):
+        self.file_name = file_name
         # TODO:  Finish this initialization method. The file_name passed should be the
         # full file name of the file that the logs will be written to.
-        pass
+        
 
     # The methods below are just suggestions. You can rearrange these or 
     # rewrite them to better suit your code style. 
@@ -22,23 +23,34 @@ class Logger(object):
 
     def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
                        basic_repro_num):
+        # open in write mode
+        with open(self.file_name, 'w') as data_file:
+            data_file.write(f"Population Size: {pop_size}\tVaccination Percentage: {vacc_percentage}\tVirus Name: {virus_name}\tMortality Rate: {mortality_rate}\tReproduction Rate: {basic_repro_num}\n")
+        
         # TODO: Finish this method. This line of metadata should be tab-delimited
         # it should create the text file that we will store all logs in.
         # TIP: Use 'w' mode when you open the file. For all other methods, use
         # the 'a' mode to append a new log to the end, since 'w' overwrites the file.
         # NOTE: Make sure to end every line with a '/n' character to ensure that each
         # event logged ends up on a separate line!
-        pass
+        
 
     def log_interactions(self, step_number, number_of_interactions, number_of_new_infections):
+        with open(self.file_name, 'a') as data_file:
+            data_file.write(f"Step {step_number}: Interactions: {number_of_interactions}, New Infections: {number_of_new_infections}\n")
+
+
         # TODO: Finish this method. Think about how the booleans passed (or not passed)
         # represent all the possible edge cases. Use the values passed along with each person,
         # along with whether they are sick or vaccinated when they interact to determine
         # exactly what happened in the interaction and create a String, and write to your logfile.
-        pass
+        
 
     def log_infection_survival(self, step_number, population_count, number_of_new_fatalities):
         # TODO: Finish this method. If the person survives, did_die_from_infection
         # should be False.  Otherwise, did_die_from_infection should be True.
         # Append the results of the infection to the logfile
-        pass
+        # open in append mode
+        with open(self.file_name, 'a') as data_file:
+            data_file.write(f"Step {step_number}: Population Count: {population_count}, New Fatalities: {number_of_new_fatalities}\n")
+
